198febf0c2bbe15dc4cf485f586aff2b6676164e
vineetkapoor <kapoor.vnt@gmail.com>
2018-01-07 16:49:09 -0500

    complete vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,32 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+// 	let num = 0;
+// 	let checker = ['a', 'e', 'i', 'o', 'u'];
+//
+// 	for (let char of str.toLowerCase()) {
+// 		if (checker.includes(char)) {
+// 			num++;
+// 		}
+// 	}
+// 	return num;
+// }
+
+// function vowels(str) {
+// 	let num = 0;
+// 	const splittedString = str.toLowerCase().split('');
+// 	splittedString.forEach(elem => {
+// 		if (['a', 'e', 'i', 'o', 'u'].indexOf(elem) > -1) {
+// 			num += 1;
+// 		}
+// 	});
+// 	return num;
+// }
