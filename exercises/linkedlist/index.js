b7bca8aac082ec281c3f19e2e64480eb08dc9c85
vineetkapoor <kapoor.vnt@gmail.com>
2018-01-21 11:56:19 -0500

    Linked list unit tests

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,82 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, node = null) {
+		this.data = data;
+		this.next = node;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+	}
+
+	size() {
+		let currentNode = this.head;
+		let length = 0;
+		while (currentNode) {
+			length++;
+			currentNode = currentNode.next;
+		}
+		return length;
+	}
+
+	getFirst() {
+		return this.head;
+	}
+
+	getLast() {
+		if (!this.head) {
+			return null;
+		}
+		let currentNode = this.head;
+
+		while (currentNode) {
+			if (!currentNode.next) {
+				return currentNode;
+			}
+			currentNode = currentNode.next;
+		}
+	}
+
+	clear() {
+		this.head = null;
+	}
+
+	removeFirst() {
+		if (!this.head) {
+			return;
+		}
+		this.head = this.head.next;
+	}
+
+	removeLast() {
+		if (!this.head) {
+			return null;
+		}
+		if (this.size() === 1) {
+			this.head = null;
+			return;
+		}
+		let currentNode = this.head.next;
+		let prevNode = this.head;
+
+		while (currentNode) {
+			if (!currentNode.next) {
+				prevNode.next = null;
+				currentNode = null;
+				return;
+			}
+			prevNode = currentNode;
+			currentNode = currentNode.next;
+		}
+	}
+}
 
 module.exports = { Node, LinkedList };
